053e6bda651500ab9d923f9ae162d28b5f7426e1
glebvy <gleb.virich9@gmail.com>
2024-05-07 21:01:37 +0200

    insertNode finalized

--- BTree_realization.c
+++ BTree_realization.c
@@ -11,3 +11,26 @@ void createNode(Node* node,int value) {
 	}
 }
 
+Node* insertNode(Node* root, int value, int time) {
+    if (root == NULL) {
+        Node* newNode = (Node*)malloc(sizeof(Node));
+        if (newNode != NULL) {
+            newNode->value = value;
+            newNode->time = time;
+            newNode->left = NULL;
+            newNode->right = NULL;
+            return newNode;
+        }
+        else {
+            fprintf(stderr, "Memory allocation error.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    if (value < root->value) {
+        root->left = insertNode(root->left, value, time);
+    }
+    else if (value > root->value) {
+        root->right = insertNode(root->right, value, time);
+    }
+    return root;
+}
