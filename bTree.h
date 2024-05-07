fe84fac076d43cad715e126a329cd7e7d89c5966
glebvy <gleb.virich9@gmail.com>
2024-05-07 20:46:51 +0200

    // Insert a node into the tree // Removing a node from the tree // Search for a node in the tree by value // Print all nodes of the tree in ascending order // Freeing memory occupied by tree nodes
    in progress

--- bTree.h
+++ bTree.h
@@ -9,6 +9,28 @@ typedef struct Node {
     struct Node* right;
 } Node;
 
+// Create a new tree node
+Node* createNode(int value, int time);
+
+// Insert a node into the tree
+Node* insertNode(Node* root, int value, int time);
+
+// Removing a node from the tree
+Node* deleteNode(Node* root, int value);
+
+// Search for a node in the tree by value
+Node* searchNode(Node* root, int value);
+
+// Print all nodes of the tree in ascending order
+void printInOrder(Node* root);
+
+// Freeing memory occupied by tree nodes
+void freeTree(Node* root);
+
+
+typedef struct BTree {
+    Node* root;
+} Btree;
 
 
 #endif 
